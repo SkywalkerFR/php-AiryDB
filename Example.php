ec4d8939c51377085c30d9b41474d55fca73ab50
SkyEmie_ <29162548+SkyEmie@users.noreply.github.com>
2021-05-16 07:12:01 +0200

    Update Example.php

--- Example.php
+++ Example.php
@@ -23,21 +23,21 @@ if (db_exist('user')) {
 	db_create('user');
 }
 
-db_add_dir('user', '/today/SkywalkerFR/privacy');
-db_add_value('user', '/today/SkywalkerFR', 'email', 'jambon@gmail.com');
-db_add_value('user', '/today/SkywalkerFR', 'phone', '0123456789');
-db_add_value('user', '/today/SkywalkerFR', 'rip', 'so_sad');
-db_add_value('user', '/today/SkywalkerFR/privacy', 'hash_password', 'SXQncyBhIHByYW5rIGJybyAhISB4RA==');
+db_add_dir('user', '/today/user123/privacy');
+db_add_value('user', '/today/user123', 'email', 'jambon@gmail.com');
+db_add_value('user', '/today/user123', 'phone', '0123456789');
+db_add_value('user', '/today/user123', 'rip', 'so_sad');
+db_add_value('user', '/today/user123/privacy', 'hash_password', 'SXQncyBhIHByYW5rIGJybyAhISB4RA==');
 
 db_add_dir('user', '/today/Couscous/garbit');
 
 db_add_dir('user', '/today/Couscous/rip');
-db_del_dir('user', '/today/Couscous/rip'); // For the science, RIP
+db_del_dir('user', '/today/Couscous/rip'); /* For the science.. */
 
 db_del_value('user', '/today/SkywalkerFR', 'rip');
 
 $arrayUser1 = array(
-	'username' => 'xX_D4rkM1cH3L_Xx',
+	'username' => 'Arthur',
 	'perm'     => '0x02',
 	'border'   => '#00ff00',
 	'pp'       => '/var/www/html/data/pp_123.png',
@@ -45,7 +45,7 @@ $arrayUser1 = array(
 );
 
 $arrayUser2 = array(
-	'username' => 'Ozachi',
+	'username' => 'Elodie',
 	'perm'     => '0x01',
 	'border'   => '#112233',
 	'pp'       => '/var/www/html/data/pp_1221.png',
@@ -56,15 +56,15 @@ db_add_value('user', NULL, NULL, $arrayUser1);
 db_add_value('user', NULL, NULL, $arrayUser2);
 
 
-$hash_password = db_get_value('user', '/today/SkywalkerFR/privacy', 'hash_password');
-$user_array    = db_get_dir('user', '/today/SkywalkerFR');
+$hash_password = db_get_value('user', '/today/user123/privacy', 'hash_password');
+$user_array    = db_get_dir('user', '/today/user123');
 $db_content    = db_get_all('user');
 
 
 
 echo 'Hash password : <pre>'.$hash_password.'</pre><hr>';
 
-echo 'SkywalkerFR\'s array : <pre>';
+echo 'user123\'s array : <pre>';
 print_r($user_array);
 echo '</pre><hr>';
 
